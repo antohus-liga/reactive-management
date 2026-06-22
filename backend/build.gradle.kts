54f8090d32c26d0b8a0925c059cc585eb2b21217
antohus-liga <antonioslotylo0904@gmail.com>
2026-06-22 17:28:50 +0100

    add swagger ui for api documentation

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
