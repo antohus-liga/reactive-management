8897b416a0e2478f4f2f805cfabb0d5de0aac237
antohus-liga <antonioslotylo0904@gmail.com>
2026-07-01 09:52:24 +0100

    add validation to materials dtos

--- backend/src/main/kotlin/org/napetrico/backend/features/materials/dto/UpdateMaterialRequest.kt
+++ backend/src/main/kotlin/org/napetrico/backend/features/materials/dto/UpdateMaterialRequest.kt
@@ -1,14 +1,23 @@
 package org.napetrico.backend.features.materials.dto
 
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.Size
 import org.napetrico.backend.common.enums.MeasurementType
 import java.math.BigDecimal
 import java.util.UUID
 
 data class UpdateMaterialRequest(
+
+    @field:Size(max = 90)
     override val description: String,
+
     override val categoryPublicId: UUID,
     override val measurement: MeasurementType,
+
+    @field:Min(value = 0, message = "Unit price cannot be negative.")
     override val unitPrice: BigDecimal,
+
+    @field:Min(value = 0, message = "Quantity cannot be negative.")
     val quantity: Int,
 ) : MaterialRequest(
     description,
