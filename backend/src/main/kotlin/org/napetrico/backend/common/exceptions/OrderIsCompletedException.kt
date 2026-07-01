b00ceb4abf2893d53010fcd2ae7558adfa9e56de
antohus-liga <antonioslotylo0904@gmail.com>
2026-07-01 09:13:39 +0100

    fix english

--- backend/src/main/kotlin/org/napetrico/backend/common/exceptions/OrderIsCompletedException.kt
+++ backend/src/main/kotlin/org/napetrico/backend/common/exceptions/OrderIsCompletedException.kt
@@ -1,5 +1,5 @@
 package org.napetrico.backend.common.exceptions
 
 class OrderIsCompletedException : RuntimeException(
-    "The order has already been completed. You can't neither add movements to it nor complete it again."
+    "The order has already been completed. You can neither add movements to it nor complete it again."
 )
