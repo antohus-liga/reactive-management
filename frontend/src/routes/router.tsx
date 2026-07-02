5d45dc1091b3f0489bb462cd2fc020b2c425085f
antohus-liga <antonioslotylo0904@gmail.com>
2026-07-02 16:56:18 +0100

    add dashboard page to routes

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -3,10 +3,38 @@ import WelcomePage from "@/pages/WelcomePage";
 import NotFoundPage from "@/pages/NotFoundPage";
 import {SignInPage} from "@/features/auth/SignInPage.tsx";
 import {RegisterPage} from "@/features/auth/RegisterPage.tsx";
+import PublicOnlyRoute from "@/routes/PublicOnlyRoute.tsx";
+import PublicShell from "@/components/layout/PublicShell.tsx";
+import ProtectedRoute from "@/routes/ProtectedRoute.tsx";
+import AppShell from "@/components/layout/AppShell.tsx";
+import {DashboardPage} from "@/features/dashboard/DashboardPage.tsx";
 
 export const router = createBrowserRouter([
-    { path: "/", element: <WelcomePage /> },
-    { path: "/signin", element: <SignInPage /> },
-    { path: "/register", element: <RegisterPage /> },
+    {
+        element: <PublicOnlyRoute/>,
+        children: [
+            {
+                element: <PublicShell/>,
+                children: [
+                    { path: "/", element: <WelcomePage /> },
+                    { path: "/signin", element: <SignInPage /> },
+                    { path: "/register", element: <RegisterPage /> },
+                ]
+            }
+        ]
+    },
+
+    {
+        element: <ProtectedRoute/>,
+        children: [
+            {
+                element: <AppShell/>,
+                children: [
+                    { path: "/dashboard", element: <DashboardPage/> },
+                ]
+            },
+        ]
+    },
+
     { path: "*", element: <NotFoundPage /> }, // catch-all for unmatched URLs
 ]);
