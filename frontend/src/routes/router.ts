170a2879847f1ab70469be9d51bdb883e121aa7d
antohus-liga <antonioslotylo0904@gmail.com>
2026-07-02 11:53:48 +0100

    add query client provider

--- frontend/src/routes/router.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import WelcomePage from "@/pages/WelcomePage";
-import NotFoundPage from "@/pages/NotFoundPage";
-
-export const router = createBrowserRouter([
-    { path: "/", element: WelcomePage() },
-    { path: "*", element: NotFoundPage() }, // catch-all for unmatched URLs
-]);
