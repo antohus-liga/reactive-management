e628a673d5a56b146100d0af32e7633c45b761a8
antohus-liga <antonioslotylo0904@gmail.com>
2026-07-06 16:27:53 +0100

    fix layout issue, page is always scrollable

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <div className={"bg-gray-900 text-white bg-[repeating-linear-gradient(45deg,#151318,#182138_20px,transparent_0,transparent_50%)] bg-size-[60px_60px] bg-fixed p-4"}>
+            <div className={"bg-gray-900 text-white bg-[repeating-linear-gradient(45deg,#151318,#182138_20px,transparent_0,transparent_50%)] bg-size-[60px_60px] bg-fixed p-4 min-h-screen max-h-full"}>
                 <RouterProvider router={router}/>
             </div>
         </QueryClientProvider>
