74c9d76630b318fef8a6345b715dbf928bade68b
antohus-liga <antonioslotylo0904@gmail.com>
2026-07-08 17:29:25 +0100

    fix validations for product dtos

--- backend/src/main/kotlin/org/napetrico/backend/features/products/dto/CreateProductRequest.kt
+++ backend/src/main/kotlin/org/napetrico/backend/features/products/dto/CreateProductRequest.kt
@@ -1,6 +1,6 @@
 package org.napetrico.backend.features.products.dto
 
-import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.DecimalMin
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 import org.napetrico.backend.common.enums.MeasurementType
@@ -14,10 +14,17 @@ data class CreateProductRequest(
     override val categoryPublicId: UUID,
     override val measurement: MeasurementType,
 
-    @field:Min(value = 0, message = "Fixed price cannot be negative.")
+    @field:DecimalMin(
+        value = "0.01",
+        inclusive = true,
+        message = "Fixed price cannot be negative."
+    )
     override val fixedPrice: BigDecimal? = null,
 
-    @field:Pattern(regexp = "^\\d+(\\.\\d+)?%$", message = "Fixed price cannot be negative.")
+    @field:Pattern(
+        regexp = "^(?:0\\.\\d*[1-9]\\d*|[1-9]\\d*(?:\\.\\d+)?)%$",
+        message = "Selling margin must be greater than 0% and end with '%'."
+    )
     override val sellingMargin: String? = null,
 ) : ProductRequest(
     description = description,
