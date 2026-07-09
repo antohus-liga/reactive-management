f6d6a31edd76e8e1f70afe779ae0437870420b6a
antohus-liga <antonioslotylo0904@gmail.com>
2026-07-09 14:15:20 +0100

    allow empty recipes

--- backend/src/main/kotlin/org/napetrico/backend/features/products/dto/ProductRecipeRequest.kt
+++ backend/src/main/kotlin/org/napetrico/backend/features/products/dto/ProductRecipeRequest.kt
@@ -1,10 +1,8 @@
 package org.napetrico.backend.features.products.dto
 
 import jakarta.validation.Valid
-import jakarta.validation.constraints.NotEmpty
 
 data class ProductRecipeRequest(
-    @field:NotEmpty
     @field:Valid
     val ingredients: Set<MaterialIngredientRequest>,
 )
