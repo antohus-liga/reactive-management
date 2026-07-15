42b0fae4f0f544f404eb896644fba2e65bae0cbc
antohus-liga <antonioslotylo0904@gmail.com>
2026-07-15 16:14:15 +0100

    return error codes

--- backend/src/main/kotlin/org/napetrico/backend/common/exceptions/OrderHasNoMovementsException.kt
+++ backend/src/main/kotlin/org/napetrico/backend/common/exceptions/OrderHasNoMovementsException.kt
@@ -1,5 +1,5 @@
 package org.napetrico.backend.common.exceptions
 
 class OrderHasNoMovementsException : RuntimeException(
-    "This order cannot be completed due to having no movements attached to it."
+    "error.orderHasNoMovements"
 )
--- backend/src/main/kotlin/org/napetrico/backend/common/exceptions/OrderIsCompletedException.kt
+++ backend/src/main/kotlin/org/napetrico/backend/common/exceptions/OrderIsCompletedException.kt
@@ -1,5 +1,5 @@
 package org.napetrico.backend.common.exceptions
 
 class OrderIsCompletedException : RuntimeException(
-    "The order has already been completed."
+    "error.orderCompleted"
 )
